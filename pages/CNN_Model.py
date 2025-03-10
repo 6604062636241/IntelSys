23f5a076c8ae4549e8f8eeee0f2b7a0a527b0b0c
6604062636241 <s6604062636241@email.kmutnb.ac.th>
2025-03-11 02:15:41 +0700

    Add

--- pages/CNN_Model.py
+++ pages/CNN_Model.py
@@ -4,29 +4,29 @@ import tensorflow as tf
 import numpy as np
 import gdown
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 st.set_page_config(page_title="Neural Network Model", layout="wide")
 
 file_id = "1Sq71teUnGS7yseEdSLErvE3rA5rEfvmS"
 output = "food101_model.h5"
-
 gdown.download(f"https://drive.google.com/uc?id={file_id}", output, quiet=False)
-model = tf.keras.models.load_model("food101_model.h5")
 
+model = tf.keras.models.load_model("food101_model.h5")
 
 class_names_file = "classes.txt"
 with open(class_names_file, "r") as file:
     class_names = [line.strip() for line in file.readlines()]
 
+print(class_names)
+
 def predict_image(img):
-    img = img.resize((224, 224))
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    img = img.resize((224, 224)) 
+    img_array = np.array(img) / 255.0 
+    img_array = np.expand_dims(img_array, axis=0) 
 
     prediction = model.predict(img_array)
     predicted_class = class_names[np.argmax(prediction)]
-    confidence = np.max(prediction)
+    confidence = np.max(prediction)  
 
     return predicted_class, confidence
 
@@ -39,5 +39,6 @@ if uploaded_file is not None:
     st.image(img, caption="Uploaded successfully", use_container_width=True)
 
     result, conf = predict_image(img)
+
     st.write(f"Predicted class: {result}")
     st.write(f"Confidence: {conf:.2%}")
