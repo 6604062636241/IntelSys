8564c41abcd951c900fa96dee710b2584daa28d3
6604062636241 <s6604062636241@email.kmutnb.ac.th>
2025-03-16 15:55:15 +0700

    add

--- pages/Machines_Information.py
+++ pages/Machines_Information.py
@@ -1,36 +1,34 @@
 import streamlit as st
 
-st.title("SVM Model for Income Level Prediction")
+st.title("svm info")
 
-st.header("1. Data Preparation")
-st.write("""The dataset used for the model development is the Adult Census Income Dataset. 
-This dataset contains economic and social factors, such as age, education level, 
-occupation, and nationality, that influence income level.""")
+st.write("ผมเริ่มโดยการหา dataset ในเว็บ kaggle แล้วไปเจออันที่น่าสนใจคือ adult dataset ซึ่งเป็นข้อมูลเกี่ยวกับข้อมูลพื้นฐานของคนเช่น อายุ ชนิดการทำงาน ระดับการศึกษา สถานะสมรส อาชีพ")
+st.image("./imgs/11.png", caption="", use_container_width=True)
 
-st.header("2. SVM Algorithm Theory")
-st.write("""Support Vector Machine (SVM) is an algorithm that uses the principle of a hyperplane 
-to separate data into distinct classes. It selects the hyperplane that maximizes 
-the margin between the nearest data points of each class, which helps improve 
-the model’s ability to generalize. SVM can be used for problems where data is linearly 
-separable or for more complex problems where the Kernel Trick is applied to transform 
-the data into a higher-dimensional space for easier separation.""")
+st.write("ทำการโหลดโฟลเดอร์ adult.csv และเลือกเฉพาะคอลัมน์ที่ต้องการมี age workclass education marital.status occupation relationship race native.country income")
+st.image("./imgs/12.png", caption="", use_container_width=True)
 
-st.header("3. SVM Model Development")
-st.subheader("3.1 Data Splitting into Training and Testing Sets")
-st.write("""The data is split into a Training Set and a Test Set. The training set allows the model 
-to learn from labeled examples, while the test set is used to evaluate the model’s performance 
-on unseen data.""")
+st.write("แปลงเป็นค่าตัวเลขโดยใช้ LabelEncoder เพื่อให้โมเดลใช้งานได้และค่าที่แปลงแล้วจะเก็บใน DataFrame และบันทึก LabelEncoder ในแต่ละคอลัมน์ไว้ใน encoder_dict เพื่อใช้ทีหลัง")
+st.image("./imgs/13.png", caption="", use_container_width=True)
 
-st.subheader("3.2 Training the SVM Model")
-st.write("""The Linear Kernel SVM is chosen because the income level classification is expected to be 
-linearly separable. The model learns from the training data and constructs the best hyperplane 
-to classify new data.""")
+st.write("คอลัมน์ income เป็นคอลัมน์ที่ต้องทำนายว่าเงินเดือน <50K หรือ >50K ทำการแปลงเป็นตัวเลขด้วย LabelEncoder")
+st.image("./imgs/14.png", caption="", use_container_width=True)
 
-st.subheader("3.3 Model Evaluation")
-st.write("""After training, the model's accuracy is evaluated by comparing its predictions against 
-the actual values in the test set. This helps determine how well the model generalizes to 
-new, unseen data.""")
+st.write("เอาค่าของคอลัมน์ age ทำการ Standardized ด้วย StandardScaler เพื่อให้มีค่าเป็น 0 และ SD เป็น 1 ช่วยเพิ่มประสิทธิภาพของโมเดล")
+st.image("./imgs/15.png", caption="", use_container_width=True)
 
-st.subheader("3.4 Saving the Model")
-st.write("""Once the model is trained, the SVM model and the associated transformers are saved 
-as a file for future use.""")
+st.write("บันทึกโมเดล สเกลเลอร์ ตัวแปลงค่าของฟีเจอร์ ตัวแปลงของคอลัมน์ income ไว้ใช้ทีหลัง")
+st.image("./imgs/16.png", caption="", use_container_width=True)
+
+
+
+st.title("randomForest info")
+
+st.write("ผมใช้ smote สำหรับสร้างข้อมูลจำลองในคลาสที่มีข้อมูลน้อย เพื่อแก้ปัญหาข้อมูลที่ไม่สมดุล")
+st.image("./imgs/17.png", caption="", use_container_width=True)
+
+st.write("ใช้ GridSearchCV เพื่อหาค่าพารามิเตอร์ที่ดีที่สุดสำหรับโมเดล โดยทดสอบค่าพารามิเตอร์ต่างๆ เช่น จำนวนต้นไม้ ความลึกสูงสุดของต้นไม้ จำนวนข้อมูลขั้นต่ำในการแบ่ง จำนวนข้อมูลขั้นต่ำในแต่ละใบไม้ และทำการเลือกค่าพารามิเตอร์ที่ให้ความแม่นยำสูงสุด")
+st.image("./imgs/18.png", caption="", use_container_width=True)
+
+st.write("save โมเดล สเกลเลอร์ ตัวแปลงค่าของฟีเจอร์ ตัวแปลงของคอลัมน์ income เพื่อให้นำไปใช้ทีหลังได้")
+st.image("./imgs/19.png", caption="", use_container_width=True)
