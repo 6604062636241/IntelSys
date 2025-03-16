3c97ce1f20db7ee19ff58a20fd09318f45345e87
6604062636241 <s6604062636241@email.kmutnb.ac.th>
2025-03-16 11:23:59 +0700

    add

--- pages/Machines_Model.py
+++ pages/Machines_Model.py
@@ -5,8 +5,8 @@ import gdown
 
 try:
     gdown.download("https://drive.google.com/uc?id=1tfyV__6kbfgYjhtelc9dM3yJqAlgKD_d",'random_forest_model.pkl', quiet=False)
-    svm_model = joblib.load('random_forest_model.pkl')
-    rf_model = joblib.load('rf_model.pkl') 
+    svm_model = joblib.load('svm_model.pkl')
+    rf_model = joblib.load('random_forest_model.pkl') 
     scaler = joblib.load('scaler.pkl')  
     encoder_dict = joblib.load('encoders.pkl')
     target_encoder = joblib.load('target_encoder.pkl')
